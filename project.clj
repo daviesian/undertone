6eb411a3ac1f907e51a276623d159b07f923d55b
Ian Davies <github@iandavies.org>
2012-07-11 17:32:01 +0100

    Change deps now that overtone snapshot provides native scsynth.

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject undertone "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.8.0-SNAPSHOT"]
-                 [clj-native "0.9.2-SNAPSHOT"]
-                 [overtone/scsynth "3.5.3-SNAPSHOT"]])
+                 [overtone "0.8.0-SNAPSHOT"]])
